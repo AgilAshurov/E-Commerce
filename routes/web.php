d2cb0d59d8aa2b5baeb62512f2abccf8ebd90b82
AgilAshurov <agil.ashurov@gmail.com>
2026-04-14 13:47:24 +0400

    Test Resource Controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\TestController;
+use App\Http\Controllers\TestResourceController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', [TestController::class, 'index'])->name('test');
+
+
+Route::resource('/test-resource2', TestResourceController::class)->names('test2');
